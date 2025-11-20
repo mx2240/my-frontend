ccb640f18ca371ccc60c51e44007190424584d52
mx2240 <lovemicky10@gmail.com>
2025-11-20 13:01:00 +0000

    Fix vercel.json build setup

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import './css/Auth.css';
 
 
 
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 import AdminLayout from './layouts/AdminLayout'
