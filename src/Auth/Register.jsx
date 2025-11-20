b83d62c751972eaa1122e706281b4760228ebfa7
mx2240 <lovemicky10@gmail.com>
2025-11-20 23:12:51 +0000

    up

--- src/Auth/Register.jsx
+++ src/Auth/Register.jsx
@@ -129,7 +129,7 @@ const Register = () => {
         e.preventDefault();
 
         try {
-            const res = await fetch.post("/auth", {
+            const res = await fetch.post("/register", {
                 body: JSON.stringify(form),
             });
 
