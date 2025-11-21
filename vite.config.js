12df06d0149d0780ef55e9706b31f82a5ccf2016
mx2240 <lovemicky10@gmail.com>
2025-11-21 21:14:30 +0000

    up

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,17 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://express-js-on-vercel-mu-orpin.vercel.app/api",
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  },
   plugins: [
     tailwindcss(),
   ],
 })
-
