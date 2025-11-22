e12a84ea6e453270449e948c398b2fa02f859a54
mx2240 <lovemicky10@gmail.com>
2025-11-22 00:41:52 +0000

    up

--- src/fetch.js
+++ src/fetch.js
@@ -3,17 +3,24 @@ import axios from "axios";
 const fetch = axios.create({
     baseURL: "https://express-js-on-vercel-mu-orpin.vercel.app/api",
     timeout: 50000,
-    headers: { "Content-Type": "application/json" },
+    headers: {
+        "Content-Type": "application/json"
+    }
 });
 
 // Interceptor to attach token automatically
-fetch.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token"); // get token from localStorage
-    console.log("Sending token:", token); // debug log
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`; // attach token
-    }
-    return config;
-}, (error) => Promise.reject(error));
+fetch.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("token");
+
+        if (token) {
+            config.headers["Authorization"] = `Bearer ${token}`;
+            config.headers["authorization"] = `Bearer ${token}`;  // (backup lowercase for CORS/Vercel)
+        }
+
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 export default fetch;
