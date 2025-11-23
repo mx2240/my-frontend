a74c4fa3396d062b30a80021e28a24dcef99bcc3
mx2240 <lovemicky10@gmail.com>
2025-11-23 14:00:12 +0000

    up

--- src/fetch.js
+++ src/fetch.js
@@ -13,6 +13,8 @@ fetch.interceptors.request.use(
         const token = localStorage.getItem("token"); // ensure you saved JWT in localStorage
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
+            localStorage.setItem("token", loginResponse.data.token);
+
         }
         return config;
     },
