93240b7fff0763962a63e798c1577b3596deb93d
mx2240 <lovemicky10@gmail.com>
2025-11-23 05:46:57 +0000

    up

--- src/fetch.js
+++ src/fetch.js
@@ -10,7 +10,7 @@ const fetch = axios.create({
 });
 
 fetch.interceptors.request.use((config) => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem(token);
     if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
 }, (err) => Promise.reject(err));
