adeb8859b894a9ee74fc5f2e39193c83baa6b7f0
mx2240 <lovemicky10@gmail.com>
2025-11-24 21:25:39 +0000

    up

--- src/fetch.js
+++ src/fetch.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const fetch = axios.create({
-    baseURL: "https://my-backend-amber.vercel.app/",
+    baseURL: "https://my-backend-amber.vercel.app/api",
     timeout: 50000,
     headers: { "Content-Type": "application/json" },
 });
