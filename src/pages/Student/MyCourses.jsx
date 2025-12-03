8498fd6f003512305bfe356cf92cfd352fb5cedc
mx2240 <lovemicky10@gmail.com>
2025-12-03 15:30:21 +0000

    up

--- src/pages/Student/MyCourses.jsx
+++ src/pages/Student/MyCourses.jsx
@@ -3,7 +3,7 @@ import StudentLayout from "../../layouts/StudentLayout";
 import { AuthContext } from "../../context/AuthProvider";
 import { Link } from "react-router-dom";
 import toast from "react-hot-toast";
-// import fetch from "../../fetch"
+import fetch from "../../fetch"
 
 const MyCourses = () => {
     const { token } = useContext(AuthContext);
