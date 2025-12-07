2fb6b28e0d69f7e9190a7c8c8bcf3e4a80deb806
mx2240 <lovemicky10@gmail.com>
2025-12-07 15:02:44 +0000

    up

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import './css/Auth.css';
 import './css/Register.css';
 import './css/Fees.css';
 import './css/AdminDashboard.css'
-import './css/StudentFees.css'
+import './css/studentFees.css'
 
 
 
