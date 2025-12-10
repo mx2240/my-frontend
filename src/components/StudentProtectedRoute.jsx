e8a1fe3188c3ebbaa2e89a5887e4523bff0b59f8
mx2240 <lovemicky10@gmail.com>
2025-12-10 15:49:18 +0000

    up

--- src/components/StudentProtectedRoute.jsx
+++ src/components/StudentProtectedRoute.jsx
@@ -1,16 +1,16 @@
 import { Navigate } from "react-router-dom";
 
 export default function StudentProtectedRoute({ children }) {
-    const token = localStorage.getItem("studentToken");
-    const user = JSON.parse(localStorage.getItem("student"));
+    const studentToken = localStorage.getItem("studentToken");
+    const student = JSON.parse(localStorage.getItem("student"));
 
     // Not logged in at all
-    if (!token || !user) {
+    if (!studentToken || !student) {
         return <Navigate to="/student/login" replace />;
     }
 
     // Must be a student
-    if (!user.role || user.role !== "student") {
+    if (!student.role || student.role !== "student") {
         return <Navigate to="/student/login" replace />;
     }
 
