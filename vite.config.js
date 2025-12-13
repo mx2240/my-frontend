dfdcf7f803a107de4fc48659a941dfe758937dd5
mx2240 <lovemicky10@gmail.com>
2025-12-13 12:17:34 +0000

    up

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,35 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
+// import { defineConfig } from 'vite'
+// import tailwindcss from '@tailwindcss/vite'
+
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://express-js-on-vercel-mu-orpin.vercel.app/api",
+//         changeOrigin: true,
+//         secure: false,
+//       }
+//     }
+//   },
+//   plugins: [
+//     tailwindcss(),
+//   ],
+// })
+
+
+
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  plugins: [react()],
   server: {
     proxy: {
       "/api": {
         target: "https://express-js-on-vercel-mu-orpin.vercel.app/api",
         changeOrigin: true,
         secure: false,
-      }
-    }
+      },
+    },
   },
-  plugins: [
-    tailwindcss(),
-  ],
-})
+});
